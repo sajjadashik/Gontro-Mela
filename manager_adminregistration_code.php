75ffb29b822e084d747be0da4ae756604086dba3
Muhtasim <57425467+MuhtasimRiyon@users.noreply.github.com>
2021-11-19 20:23:15 +0600

    Create manager_adminregistration_code.php

---
+++ manager_adminregistration_code.php
@@ -0,0 +1,25 @@
+<?php
+require_once('Connection.php');
+$name = $uName = $pass = $cPass = $gender = $email = $phone = $address = '';
+
+$name = $_POST['name'];
+$uName = $_POST['uname'];
+$pass = $_POST['password'];
+$cPass = $_POST['cpassword'];
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+$phone = $_POST['phonenumber'];
+$address = $_POST['address'];
+
+
+$sql = "INSERT INTO admn_regi(Name,Username,Password,cPassword,Gender,Email,Phone,Address) VALUES ('$name','$uName','$pass','$cPass','$gender','$email','$phone','$address')";
+$result = mysqli_query($conn, $sql);
+if($result)
+{
+	header("Location: login.php");
+}
+else
+{
+	echo "Error :".$sql;
+}
+?>
