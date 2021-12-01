13ce18183075b2e5abab7f0fa74536d370436929
shakil hossen <shakilhossen566@gmail.com>
2021-12-01 22:06:57 +0600

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,9 +1,29 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Registration</title>
+<!doctype html>
+<html lang="en">
 
-        <?php
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+   
+    <!-- font awsome -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    
+  
+    <link rel="stylesheet" href="/CLASS_1/style.css" >
+    <!-- title icon -->
+    <link rel="shortcut icon" type="image/png" href="/CLASS_1/image/logo.jpg">
+    <title>Registration</title>
+
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    
+    <?php
         $name="";
         $err_name="";
         $uname="";
@@ -125,53 +145,66 @@
         ?>
     </head>
 
-    <body>
-        <fieldset>
-            <legend align="center"><h1>Registration</h1></legend>
+
+  <body>
+  <?php include('header.php'); ?>
+
+<br> <br> <br> <br>
+<div class="regis ">
+            <legend ><h1>Registration</h1></legend>
                 <form action="registration_code.php" method="post">
-                    <table align="center">
+                    <table >
                         <tr>
                             <td><span>Name:</span> </td>
-                            <td><input type="text" value="" placeholder="Name" name="name"><br> <span style="color: red;"><?php echo $err_name;?> </span> </td>
+                            <td><input class="txt_field" type="text" value="" placeholder="Name" name="name"><br> <span style="color: red;"><?php echo $err_name;?> </span> </td>
                         </tr>
 
                         <tr>
                             <td><span>Username:</span> </td>
-                            <td><input type="text" value="" placeholder="Username" name="uname"><br> <span style="color: red;"><?php echo $err_uname;?> </span> </td>
+                            <td><input class="txt_field" type="text" value="" placeholder="Username" name="uname"><br> <span style="color: red;"><?php echo $err_uname;?> </span> </td>
                         </tr>
                         
                         <tr>
                             <td><span>Password:</span></td>
-                            <td><input type="password" placeholder="Password" name="password"><br><span style="color: red;"><?php echo $err_pass;?> </span></td>
+                            <td><input class="txt_field" type="password" placeholder="Password" name="password"><br><span style="color: red;"><?php echo $err_pass;?> </span></td>
                         </tr>
 
                         <tr>
                             <td><span>Confirm Password:</span></td>
-                            <td><input type="password" placeholder="Confirm Password" name="cpassword"><br> <span style="color: red;"><?php echo $err_cpass;?> </span></td>
+                            <td><input class="txt_field" type="password" placeholder="Confirm Password" name="cpassword"><br> <span style="color: red;"><?php echo $err_cpass;?> </span></td>
                         </tr>
                         <tr>
                             <td><span>Gender:</span></td>
-                            <td><input type="radio" value="Male" name="gender">Male<input type="radio" value="Female" name="gender">Female <br> <span style="color: red;"><?php echo $err_gender; ?></span> </td>
+                            <td><input class="gender"  type="radio" value="Male" name="gender">Male &nbsp<input type="radio" value="Female" name="gender">Female <br> <span style="color: red;"><?php echo $err_gender; ?></span> </td>
                         </tr>
                         <tr>
                             <td><span>Email:</span></td>
-                            <td><input type="email" placeholder="Email" value="" name="email"><br> <span style="color: red;"><?php echo $err_email;?> </span></td>
+                            <td><input class="txt_field" type="email" placeholder="Email" value="" name="email"><br> <span style="color: red;"><?php echo $err_email;?> </span></td>
                         </tr>
 
                         <tr>
                             <td><span>Phone Number:</span></td>
-                            <td><input  type="text" placeholder="Phone Number" value="" name="phonenumber"><br> <span style="color: red;"><?php echo $err_number;?> </span></td>
+                            <td><input class="txt_field"  type="text" placeholder="Phone Number" value="" name="phonenumber"><br> <span style="color: red;"><?php echo $err_number;?> </span></td>
                         </tr>
                         <tr>
                             <td><span>Address:</span></td>
-                            <td><textarea placeholder="Address" value="" name="address"></textarea><br> <span style="color: red;"><?php echo $err_address;?> </span></td>
+                            <td><textarea class="txt_field" placeholder="Address" value="" name="address"></textarea><br> <span style="color: red;"><?php echo $err_address;?> </span></td>
                         </tr>
     
                         <tr>
-                            <td colspan="3" align="center"><input type="submit" value="Submit" name="submit">
+                            <td colspan="3" ><input class="btn-outline-success m-1 submit" type="submit" value="Submit" name="submit">
                         </tr>
                     </table>
                 </form>
-        </fieldset>
+
+    </div>      
+      <br> <br> <br>
+     
+        <?php include('footer.html'); ?>
+
+    <div>
+       
+ 
+    </div>
     </body>
 </html>
